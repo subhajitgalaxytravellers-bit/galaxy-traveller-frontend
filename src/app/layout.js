eac88f5dbbce8e78da06cbaede5ed190c803d081
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2026-02-09 13:39:20 +0530

    feat: add Google Tag Manager and Facebook Pixel tracking scripts to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,12 +68,76 @@ export default async function RootLayout({ children }) {
 
   const whatsappNumber = settings?.data?.whatsapp?.number || "";
   const footerContact = settings?.data?.footerContact || {};
+  const tracking = settings?.data?.tracking || {};
+  const gtmId = String(tracking?.gtmId || "").trim();
+  const fbPixel = String(tracking?.fbPixel || "").trim();
+  const extraScripts = String(tracking?.extraScripts || "").trim();
   const siteGlobal = globals?.data || {};
   console.log("Site Global Data:", siteGlobal);
 
   return (
     <html lang="en">
+      <head>
+        {gtmId && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtm.js?id=${encodeURIComponent(gtmId)}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                `,
+              }}
+            />
+          </>
+        )}
+        {fbPixel && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${fbPixel}');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+        )}
+        {extraScripts && (
+          <script dangerouslySetInnerHTML={{ __html: extraScripts }} />
+        )}
+      </head>
       <body className={`${poppins.className} ${poppins.variable} ${geistMono.variable}`}>
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${encodeURIComponent(gtmId)}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
+        {fbPixel && (
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${encodeURIComponent(fbPixel)}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        )}
         <ReactQueryProvider>
           <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
             {/* SITE NAV */}
